669a47f8366a631d4bdd166ce5f60205074ceac4
Ash <ash.tappin@blackpepper.co.uk>
2020-06-25 16:40:21 +0100

    Add more styling and info

--- src/components/NutritionResults.js
+++ src/components/NutritionResults.js
@@ -3,8 +3,18 @@ import React, {useState} from 'react';
 const NutritionResults = (props) => {
     console.dir(props.results);
     return (<div className="nutrients-results-container">
-        <div>Calories: {props.results.calories}</div>
-        <div>Daily calorie percentage: {props.results.totalDaily.ENERC_KCAL.quantity.toFixed(2)} %</div>
+        <div className="nutrient-info">
+            <div>Calories: {props.results.calories}</div>
+            <div>Daily percentage: {props.results.totalDaily.ENERC_KCAL.quantity.toFixed(2)} %</div>
+        </div>
+        <div className="nutrient-info">
+            <div>Carbs: {props.results.totalNutrients.CHOCDF.quantity}</div>
+            <div>Daily percentage: {props.results.totalDaily.CHOCDF.quantity.toFixed(2)} %</div>
+        </div>
+        <div className="nutrient-info">
+            <div>Protein: {props.results.totalNutrients.PROCNT.quantity}</div>
+            <div>Daily percentage: {props.results.totalDaily.PROCNT.quantity.toFixed(2)} %</div>
+        </div>
     </div>)
 };
 
