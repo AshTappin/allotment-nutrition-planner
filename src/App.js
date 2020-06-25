0d5ad07c6dbd94a1d612a87f3dbb7890d3a7ff4a
Ash <ash.tappin@blackpepper.co.uk>
2020-06-25 18:23:10 +0100

    Style text area and button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <div className="heading">Allotment Nutrition Planner</div>
+          <div className="heading">Veg Growing Nutrition Planner</div>
         <VegetableContainerInput/>
       </header>
     </div>
